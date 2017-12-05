f7adacfdfa939577702efe8201f62922c0fc6dad
NishimotoSatoshi <nishimoto.satoshi@opst.co.jp>
2017-12-05 16:19:48 +0900

    行が長すぎるので、折り返した。

--- ModExp4Bar/src/module-info.java
+++ ModExp4Bar/src/module-info.java
@@ -1,4 +1,6 @@
 module opst.exp4.bar {
-	provides jp.co.opst.java9.mod.exp4.NameService with jp.co.opst.java9.mod.exp4.bar.NameServiceBar;
+	provides jp.co.opst.java9.mod.exp4.NameService
+		with jp.co.opst.java9.mod.exp4.bar.NameServiceBar;
+
 	requires opst.exp4;
 }
--- ModExp4Foo/src/module-info.java
+++ ModExp4Foo/src/module-info.java
@@ -1,4 +1,6 @@
 module opst.exp4.foo {
-	provides jp.co.opst.java9.mod.exp4.NameService with jp.co.opst.java9.mod.exp4.foo.NameServiceFoo;
+	provides jp.co.opst.java9.mod.exp4.NameService
+		with jp.co.opst.java9.mod.exp4.foo.NameServiceFoo;
+
 	requires opst.exp4;
 }
